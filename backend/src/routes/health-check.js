c7c318e3f32f1a7a7e947007ea917301f3923d0f
mightycox <j.acox@hotmail.com>
2021-08-24 10:10:48 -0700

    adding health check endpoint

---
+++ backend/src/routes/health-check.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+class HealthCheckController {
+  constructor(express) {
+    this._router = express.Router();
+    this._router.get('/api/health', (req, res) => this.healthCheck(req, res));
+  }
+
+  get router() {
+    return this._router;
+  }
+
+  async healthCheck(req, res) {
+    res.sendStatus(200);
+  }
+}
+
+const EXPRESS = require('express');
+module.exports = new HealthCheckController(EXPRESS);
